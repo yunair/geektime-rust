488775c627224b7bab412999190fba61369d3dd9
Air <airzhaoyn@gmail.com>
2022-12-24 23:21:15 +0800

    queryer

---
+++ 06_queryer/queryer/examples/covid.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use queryer::query;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+
+    let url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv";
+
+    let sql = format!(
+        "SELECT location, total_cases, new_cases, total_deaths, new_deaths \
+    FROM {} WHERE new_deaths >= 500 ORDER BY new_cases DESC",
+        url
+    );
+
+    let df1 = query(sql).await?;
+    println!("{:?}", df1);
+    Ok(())
+}
