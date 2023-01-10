c753acffc876cd3283f0d62cd56b2648d0aaba95
Air <airzhaoyn@gmail.com>
2023-01-10 12:00:12 +0800

    lifetime

---
+++ 10_lifetime/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let s1 = String::from("Lindsey");
+    let s2 = String::from("Rosie");
+    let result = max(&s1, &s2);
+    println!("bigger one: {}", result);
+}
+fn max<'a>(s1: &'a str, s2: &'a str) -> &'a str {
+    if s1 > s2 {
+        s1
+    } else {
+        s2
+    }
+}
